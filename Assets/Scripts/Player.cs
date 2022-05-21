1e8ad0d24801ea31a9ca5eeb102b6157983978a6
mpbmke <33264670+mpbmke@users.noreply.github.com>
2022-05-20 21:33:22 -0500

    Basic player movement
    Implemented basic player movement w/speed boost using Old Input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float _speed = 5.0f;
+    [SerializeField] float _speedMultiplier = 1.5f;
+
+    bool _speedBoost;
+
+    Vector3 _moveDirection;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CalculateMovement();
+    }
+
+    private void CalculateMovement()
+    {
+        if (Input.GetButtonDown("Run"))
+            _speedBoost = true;
+
+        if (Input.GetButtonUp("Run"))
+            _speedBoost = false;
+
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var forwardInput = Input.GetAxis("Vertical");
+
+        if (!_speedBoost)
+            _moveDirection = new Vector3(horizontalInput, 0, forwardInput) * _speed * Time.deltaTime;
+
+        if (_speedBoost)
+            _moveDirection = new Vector3(horizontalInput, 0, forwardInput) * _speed * _speedMultiplier * Time.deltaTime;
+
+        transform.Translate(_moveDirection);
     }
 }
